06b5df9ae6378d59d02b21e7c396499412e6eea9
spookyactioncloseup <113103963+spookyactioncloseup@users.noreply.github.com>
2022-10-22 02:33:49 +1000

    Improved Banner and Javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-//responsive menu
-
+const banner = document.querySelector('#banner');
 const mainNavBtn = document.querySelector('.main-nav .icon');
 const mainNav = document.querySelector('.main-nav');
+banner.style.paddingTop =  mainNav.offsetHeight + "px";
 
 mainNavBtn.addEventListener('click', ev => {
     if (mainNav.classList.contains('responsive')) {
@@ -17,11 +17,16 @@ const navBtns = document.querySelectorAll('.main-nav a');
 navBtns.forEach(navBtn => {
     navBtn.addEventListener('click', ev => {
         ev.preventDefault();
+        let goHome = false;
         let targetElem = ev.target.getAttribute('href');
         console.log(targetElem);
-        if (targetElem === 'icon' || targetElem === null) return;   
+        if (targetElem === '#icon') return;
+        if (targetElem === '#home') {
+            goHome = true;
+        }  
+
         let accordian = document.querySelector(targetElem);
-        toggle(ev.target, accordian);
+        toggle(ev.target, accordian, goHome);
     });
 });
 
@@ -29,11 +34,11 @@ accordianBtns.forEach(accordianBtn => {
     accordianBtn.addEventListener('click', ev => {
         let targetElem = ev.target.getAttribute('id');
         let navBtn = document.querySelector(`a[href='#${targetElem}'`);
-        toggle(navBtn, ev.target);
+        toggle(navBtn, ev.target, false);
     });
 });
 
-function toggle(btn, accordian) {
+function toggle(btn, accordian, goHome) {
     navBtns.forEach(navBtn => {
         if (navBtn.classList.contains('active')) {
             navBtn.classList.remove('active');
@@ -41,7 +46,7 @@ function toggle(btn, accordian) {
     });
 
     btn.classList.add('active');
-
+    
     accordianBtns.forEach(accordianBtn => {
         if (accordianBtn.classList.contains('open')) {
             accordianBtn.classList.remove('open');
@@ -49,15 +54,28 @@ function toggle(btn, accordian) {
         }
     });
 
-    accordian.classList.add('open');
-    accordian.nextElementSibling.classList.remove('hidden');
+    if (!goHome) {
+        accordian.classList.add('open');
+        accordian.nextElementSibling.classList.remove('hidden');
 
-    setTimeout(() => {
-        let top = accordian.offsetTop - (mainNav.offsetHeight + 30);
-        window.scroll({
-            top: top,
-            behaviour: 'smooth'
-        });
-    },100);
+        setTimeout(() => {
+            let top = accordian.offsetTop - (mainNav.offsetHeight + 30);
+            window.scroll({
+                top: top,
+                behaviour: 'smooth'
+            });
+        },100);
+
+    } else {
+        setTimeout(() => {
+            let top = 0 - (mainNav.offsetHeight + 30);
+            window.scroll({
+                top: top,
+                behaviour: 'smooth'
+            });
+        },100);
+    }
 }
+
+
 
